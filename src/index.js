77b7cc52bc3262b51b6d4f7c32c89863a8a734e6
Jacobo <jacoboburbano2004@gmail.com>
2023-09-12 12:03:51 -0500

    first and second challenge complete

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+localStorage.setItem('pagination', '5')
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        return `<article class='Card'>
+                      <img src='${product.images[1]}' alt='${product.title}'></img>
+                      <h2>${product.title}
+                      <small>${product.price}</small>
+                      </h2>
+                </article>`
       });
+      console.log(products)
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
@@ -17,15 +24,20 @@ const getData = api => {
     })
     .catch(error => console.log(error));
 }
-
-const loadData = () => {
-  getData(API);
+const loadData = ({increase = false} = {}) => {
+  let count = Number(localStorage.getItem('pagination'))
+  if(increase){
+    count += 10
+    localStorage.setItem('pagination', JSON.stringify(count))
+  }
+     getData(API + `?offset=${count}&limit=10`);
 }
-
+loadData({increase: false})
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  // console.log(entries[0].isIntersecting)
+    entries.innerHTML = loadData({increase : true})
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
-intersectionObserver.observe($observe);
+// intersectionObserver.observe($observe);
