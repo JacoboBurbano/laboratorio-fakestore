5443ac7162c8a4ff9fd5a8b7a138d93419d3a61f
Jacobo <jacoboburbano2004@gmail.com>
2023-09-25 12:39:26 -0500

    Some new features, but need parcel the code with a bundler

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-localStorage.setItem('pagination', '5')
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
+localStorage.setItem('pagination', '0')
+
+const getData = async (api) => {
+  const response = await fetch(api)
+  const data = await response.json()
+  if(response.status == 200){
+      console.log(data)
+      let products = data;
       let output = products.map(product => {
         return `<article class='Card'>
                       <img src='${product.images[1]}' alt='${product.title}'></img>
@@ -16,28 +18,35 @@ const getData = api => {
                       </h2>
                 </article>`
       });
-      console.log(products)
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
-const loadData = ({increase = false} = {}) => {
-  let count = Number(localStorage.getItem('pagination'))
-  if(increase){
-    count += 10
-    localStorage.setItem('pagination', JSON.stringify(count))
+      $app.appendChild(newItem); 
   }
-     getData(API + `?offset=${count}&limit=10`);
+  else{
+    console.log(response.statusText)
+  }
+    
 }
-loadData({increase: false})
+loadData =  async ({increase = 5} = {}) => {
+  if(Number(localStorage.getItem('pagination')) < 200){
+    getData(API + `?offset=${increase}&limit=10`);
+  }
+  else{
+    intersectionObserver.unobserve($observe)
+    let parrafo = document.createTextNode('Todos los productos Obtenidos.')
+    $app.append(parrafo)
+  }
+}
+// loadData({increase: false})
 const intersectionObserver = new IntersectionObserver(entries => {
   // console.log(entries[0].isIntersecting)
-    entries.innerHTML = loadData({increase : true})
+  let count = Number(localStorage.getItem('pagination'))
+    count += 5
+    entries.innerHTML = loadData({increase: count})
+    localStorage.setItem('pagination', JSON.stringify(count))
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
-// intersectionObserver.observe($observe);
+intersectionObserver.observe($observe);
